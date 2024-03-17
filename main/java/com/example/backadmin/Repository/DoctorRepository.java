3e430c9ecb928ce9af16ab224357af433dd7cca4
pandeyaman2170 <Aman.Pandey@iiitb.ac.in>
2024-03-17 19:06:06 +0530

    Hospitalcontroller

--- main/java/com/example/backadmin/Repository/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backadmin.Repository;
-
-import com.example.backadmin.Entity.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface DoctorRepository extends JpaRepository<Doctor,Long> {
-
-}
