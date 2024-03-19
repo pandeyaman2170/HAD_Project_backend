3c054134b90a02cd81e680453ab70eacd9393519
= <veerendragoudatp40@gmail.com>
2024-03-19 13:04:29 +0530

    login user

--- src/main/java/com/example/teleconsultationbackend/Repository/UserRepository.java
+++ src/main/java/com/example/teleconsultationbackend/Repository/UserRepository.java
@@ -4,5 +4,6 @@ import com.example.teleconsultationbackend.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    User findByPhoneAndRole(String phone, String role);
 
 }
