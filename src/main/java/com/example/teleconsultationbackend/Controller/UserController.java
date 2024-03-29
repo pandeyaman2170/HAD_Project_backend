d44037cb365bc8cd19b55b3a875d03636df92b87
Sankalp Rai <sankalp137rai@gmail.com>
2024-03-29 12:07:36 +0530

    added get_total_users

--- src/main/java/com/example/teleconsultationbackend/Controller/UserController.java
+++ src/main/java/com/example/teleconsultationbackend/Controller/UserController.java
@@ -1,9 +1,12 @@
 package com.example.teleconsultationbackend.Controller;
 
 import com.example.teleconsultationbackend.DTO.UserLoginStatus;
+import com.example.teleconsultationbackend.Entity.User;
 import com.example.teleconsultationbackend.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -15,5 +18,10 @@ public class UserController {
     @GetMapping ("/login/{phone_num}/{role}")
     public UserLoginStatus login(@PathVariable String role, @PathVariable String phone_num){
         return userService.login(phone_num, role);
+    }
+
+    @GetMapping("/get_total_users")
+    public Long getAllUsers(){
+        return userService.getTotalUsersCount();
     }
 }
--- src/main/java/com/example/teleconsultationbackend/Repository/UserRepository.java
+++ src/main/java/com/example/teleconsultationbackend/Repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.example.teleconsultationbackend.Repository;
 
 import com.example.teleconsultationbackend.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByPhoneAndRole(String phone,String role);
+
+    @Query("SELECT COUNT(*) FROM User")
+    Long getTotalUserCount();
 
 }
